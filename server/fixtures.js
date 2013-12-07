bda7e39b60c1844ec116da8bc72c4c2769f200f9
Charles Covey-Brandt <chazcb@gmail.com>
2013-12-07 13:03:01 -0800

    Get 'em some users there ya?

--- server/fixtures.js
+++ server/fixtures.js
@@ -1,4 +1,41 @@
-var fixtures = [{
+var users = [{
+    name: "Maria Claudia Mendez, Bolivia",
+    location: "Latin America and the Caribbean",
+    interests: "Mentoring",
+    program: "2006 FORTUNE/U.S. State Department Global Women's Mentoring Partnership Alumna",
+}, {
+    name: "Phelicia Dell, Samoa",
+    location: "Latin American and the Caribbean",
+}, {
+    name: "Shatha Al-Harazi, Yemen",
+    location: "Middle East and North Africa",
+}, {
+    name: "Adimaimalaga (Adi) Tafuna'i, Samoa",
+    location: "Latin American and the Caribbean",
+    interests: "Economic Empowerment",
+}, {
+    name: "Sohini Chakraborty, India",
+    location: "Asia and the Pacific Islands",
+    interests: "Human Rights",
+}, {
+    name: "Wang Xingjuan, China",
+    location: "Asia and the Pacific Islands",
+}, {
+    name: "Roshaneh Zafar, Pakistan",
+    location: "Asia and the Pacific Islands",
+    interests: "Economic Empowerment",
+}, {
+    name: "Brigitte Dzogbenuku, Ghana",
+    location: "Africa",
+    program: "2008 FORTUNE/U.S. State Department Global Women's Mentoring Partnership Alumna",
+}, {
+    name: "Doron Shaltiel",
+    location: "Africa",
+    program: "2009 Vital Voices Entrepreneurs in Handcrafts Training Program in Kenya Alumna",
+}];
+
+
+var posts = [{
     name: 'Title',
 
 
@@ -14,8 +51,9 @@ if (Meteor.isServer) {
         this.Seeds.remove({});
 
         // Add all data from the fixtures file
-        fixtures.forEach(function (fixture) {
+        posts.forEach(function (fixture) {
             this.Seeds.insert(fixture);
         });
+
     });
 }
